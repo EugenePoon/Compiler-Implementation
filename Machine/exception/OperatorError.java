999c35403754817556a080b2a152bc27ffa3526f
PanPanPan <84629840+Pan-Dada@users.noreply.github.com>
2022-06-10 18:56:55 +0800

    Delete Machine directory

--- Machine/exception/OperatorError.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class OperatorError extends Exception {
-    public OperatorError(String msg){
-        super(msg);
-    }
-}
--- Machine/mytype/basicType.java
+++
@@ -1,7 +0,0 @@
-package mytype;
-
-
-public class basicType {
-
-}
-
